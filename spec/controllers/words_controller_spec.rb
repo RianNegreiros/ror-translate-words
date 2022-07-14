95549d9407949ec6ba44da949a82a46b4021d8a2
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-14 11:59:52 -0300

    refactor words controller spec

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,24 +2,27 @@ require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
   describe "GET /index" do
+    before { get :index }
+
     context 'when some words are present' do
+      let!(:word) { create(:word) }
+
       it 'assigns @words' do
-        word = Word.create(value: 'any', language: 'any')
-        get :index
         expect(assigns(:words)).to eq([word])
+      end
+      
+      it 'renders the index template' do
+        expect(response).to render_template(:index)
       end
     end
 
-    context 'when any words are present' do
+    context 'when there is no words present' do
       it 'assigns @words' do
-        get :index
         expect(assigns(:words)).to eq([])
       end
     end
 
-
     it 'renders the index template' do
-      get :index
       expect(response).to render_template(:index)
     end
   end
