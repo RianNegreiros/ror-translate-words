8d3beb35834ca7814f507afdd2b93a68c24737ec
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-07-15 09:11:01 -0300

    endpoint new and create tests

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe WordsController, type: :controller do
-  describe "GET /index" do
+  describe 'GET /index' do
     before { get :index }
 
     context 'when some words are present' do
@@ -24,6 +24,42 @@ RSpec.describe WordsController, type: :controller do
 
     it 'renders the index template' do
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'Get new' do
+    before { get :new }
+
+    it 'assigns @word' do
+      expect(assigns(:word)).to be_a_new(Word)
+    end
+
+    it 'renders the new template' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'Post create' do
+    subject { post :create, params: params }
+
+    context 'valid params' do
+      let(:params) do
+        { word: { value: 'any_value', language: 'any_language' } }
+      end
+
+      it 'creates new word' do
+        expect { subject }.to change(Word, :count).from(0).to(1)
+      end
+    end
+
+    context 'invalid params' do
+      let(:params) do
+        { word: { value: nil } }
+      end
+
+      it 'does not create new word' do
+        expect { subject }.not_to change(Word, :count)
+      end
     end
   end
 end
